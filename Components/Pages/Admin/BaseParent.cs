f2ca4f95d6568934c0146a986f3db0f6c2a425d5
LE NGOC THAO VY <thaovy3724@gmail.com>
2025-12-01 23:35:30 +0700

    fix merging

--- Components/Pages/Admin/BaseParent.cs
+++ Components/Pages/Admin/BaseParent.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
-<<<<<<< HEAD
 using Microsoft.JSInterop;
 using StoreBlazor.DTO.Admin;
-=======
->>>>>>> 4589f5fe6f438f0040c39c4ed8666a379b6a96e5
 
 namespace StoreBlazor.Components.Pages.Admin
 {
@@ -13,19 +10,15 @@ namespace StoreBlazor.Components.Pages.Admin
         protected T SelectedItem { get; set; } = new();
 
         protected bool IsEditMode { get; set; }
-<<<<<<< HEAD
 
         [Inject]
         protected IJSRuntime JS { get; set; } = default!;
-=======
->>>>>>> 4589f5fe6f438f0040c39c4ed8666a379b6a96e5
         protected void CloseForm()
         {
             SelectedItem = new();
             IsEditMode = false;
         }
 
-<<<<<<< HEAD
         protected async Task ShowAlertAsync(ServiceResult serviceResult)
         {
             await JS.InvokeVoidAsync("showAlert", serviceResult.Message, serviceResult.Type);
@@ -35,8 +28,5 @@ namespace StoreBlazor.Components.Pages.Admin
         {
             return await JS.InvokeAsync<bool>("showDeleteAlert");
         }
-
-=======
->>>>>>> 4589f5fe6f438f0040c39c4ed8666a379b6a96e5
     }
 }
